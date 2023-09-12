7ac36b49a6d99835327bb5db2908cdd8cda49fb2
danilo775 <danilo.uba@hotmail.com>
2023-09-12 12:35:50 -0300

    aula 11

---
+++ aula11/atv1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void maxmin(int vetor[], int n, int &maximo, int &minimo){
+  
+    for(int i=0; i< n; i++){
+        if(vetor[i] < minimo){
+            minimo = vetor[i];
+        }
+        if (vetor[i] > maximo){
+            maximo = vetor[i];
+        }
+     
+        
+    } 
+
+
+}
+
+
+int main(){
+    int vetor[5], n =5, maximo = vetor[0], minimo = vetor[0]; 
+
+    for(int i=0; i< n; i++){
+        cout << "add " << i << " num ao vetor";
+        cin >> vetor[i];
+    }
+
+    maxmin(vetor, n, maximo, minimo);
+    cout << "O  maximo e :" << maximo << endl;
+    cout << "O  minimo e :" << minimo << endl;
+
+    return 0;
+}
